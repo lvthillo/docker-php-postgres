300e608e6e2b8e8ece946e958ff33926dca0291f
lvthillo <lorenz.vanthillo@outlook.com>
2017-08-17 12:48:08 +0200

    replace hardcoded by env var

--- app/src/database.php
+++ app/src/database.php
@@ -1,7 +1,7 @@
 <?php    
 
 try {
-        $conn = new PDO('pgsql:host=my-postgres;port=5432;dbname=db','dev','test');
+        $conn = new PDO('pgsql:host='.$_ENV["HOST"] .';port='.$_ENV["PORT"] .';dbname='.$_ENV["DB"] .'',''.$_ENV["USER"] .'',''.$_ENV["PASSWORD"] .'');
 
         $conn->query("SET NAMES UTF8");
     } catch (PDOException $e) {
